ffe1d3b0f7f40a1fafe85e2ed6d67b7fccd5dac4
045200 <wsir9527@gmail.com>
2025-07-20 13:06:01 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -21,25 +21,23 @@ echo "开始下载规则..."
 
 # 定义下载链接数组（规则和白名单分别处理）
 rules=(
-  "https://raw.githubusercontent.com/qq5460168/dangchu/main/black.txt" #5460
-  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt" #大萌主
+ "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt" #大萌主
   "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt"  #DD
   "https://raw.githubusercontent.com/Cats-Team/dns-filter/main/abp.txt" #AdRules DNS Filter
-  "https://raw.hellogithub.com/hosts" #GitHub加速
+
+"https://raw.hellogithub.com/hosts" #GitHub加速
   "https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt" #测试hosts
   "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt" #白名单
-  ""
-  "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Blacklist.txt" #mphin
-  "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/black-list" #周木木
-  "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt" #liwenjie119
+    "https://lingeringsound.github.io/10007_auto/adb.txt" #10007
+
+"https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-Replenish.txt" #秋风补充
+
+"https://raw.githubusercontent.com/2771936993/HG/main/hg1.txt" #海哥
   "https://github.com/entr0pia/fcm-hosts/raw/fcm/fcm-hosts" #FCM Hosts
   "https://raw.githubusercontent.com/790953214/qy-Ads-Rule/refs/heads/main/black.txt" #晴雅
   "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt" #秋风规则
   "https://raw.githubusercontent.com/2Gardon/SM-Ad-FuckU-hosts/refs/heads/master/SMAdHosts" #下一个ID见
-  "https://raw.githubusercontent.com/tongxin0520/AdFilterForAdGuard/refs/heads/main/KR_DNS_Filter.txt" #tongxin0520
-  "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/refs/heads/main/Rules/blacklist.txt" #Zisbusy
-  "https://oss.xlxbk.cn/allow.txt" #xlxbk
-  "https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/refs/heads/main/FuLingRules/FuLingBlockList.txt" #茯苓
+ "https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/refs/heads/main/FuLingRules/FuLingBlockList.txt" #茯苓
   "https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/refs/heads/main/FuLingRules/FuLingAllowList.txt" #茯苓白名单
   "https://raw.githubusercontent.com/twoone-3/AdGuardHomeForRoot/refs/heads/main/src/bin/data/filters/1732747955.txt" #twoone-3
 )
@@ -51,7 +49,8 @@ allow=(
   "https://raw.githubusercontent.com/user001235/112/main/white.txt" #浅笑
   "https://raw.githubusercontent.com/jhsvip/ADRuls/main/white.txt" #jhsvip
   "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt" #liwenjie119
-  "https://oss.xlxbk.cn/allow.txt" #xlxbk
+  
+"https://oss.xlxbk.cn/allow.txt" #xlxbk
   "https://raw.githubusercontent.com/miaoermua/AdguardFilter/main/whitelist.txt" #喵二白名单
   "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/refs/heads/main/Rules/whitelist.txt" #Zisbusy
   "https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/refs/heads/main/FuLingRules/FuLingAllowList.txt" #茯苓
